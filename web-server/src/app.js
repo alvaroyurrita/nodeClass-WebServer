e4138d8a4b4b4bc763c855cb7c29bf77bd54e271
Alvaro Yurrita <alvro.yurrita@jenandal.com>
2022-08-21 22:00:49 -0400

    done with styiling

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,59 +1,67 @@
-const path = require('path');
-const express = require('express');
+const path = require('path')
+const express = require('express')
 const hbs = require('hbs')
 
-const app = express();
-const dirApp = path.join(__dirname, "../public");
-const dirViews = path.join(__dirname,"../templates/views")
-const dirPartials = path.join(__dirname,"../templates/partials")
+const app = express()
 
-app.set('view engine', 'hbs');
-app.set('views',dirViews)
-hbs.registerPartials(dirPartials);
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
-app.use(express.static(dirApp));
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
-  res.render('index',{
-    title: 'Weather',
-    name: 'me',
-  })
+    res.render('index', {
+        title: 'Weather',
+        name: 'Andrew Mead'
+    })
 })
 
 app.get('/about', (req, res) => {
-  res.render('about',{
-    title: 'about',
-    name: 'me',
-  })
+    res.render('about', {
+        title: 'About Me',
+        name: 'Andrew Mead'
+    })
 })
 
 app.get('/help', (req, res) => {
-  res.render('help',{
-    title: 'help',
-    helpMessage: 'this is a helpful message',
-    name: 'me',
-  })
+    res.render('help', {
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Andrew Mead'
+    })
 })
 
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Philadelphia'
+    })
+})
 
 app.get('/help/*', (req, res) => {
-  res.render('notFound',{
-    title: '404',
-    notFoundMessage: 'Help Page Not Found',
-    name: 'me',
-  })
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Help article not found.'
+    })
 })
 
 app.get('*', (req, res) => {
-  res.render('notFound',{
-    title: '404',
-    notFoundMessage: 'Page Page Not Found',
-    name: 'me',
-  })
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Page not found.'
+    })
 })
 
-
 app.listen(3000, () => {
-  console.log("server started")
+    console.log('Server is up on port 3000.')
 })
